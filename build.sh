9e1d6b6d902427a572d532264a65f8f71cbdb69d
AlexAragundiUwU <alexaragundi3050@gmail.com>
2026-01-29 15:56:21 -0500

    Update: build.sh dinámico

--- build.sh
+++ build.sh
@@ -2,14 +2,24 @@
 # exit on error
 set -o errexit
 
+# Instalar dependencias
 pip install -r requirements.txt
 
+# Recolectar archivos estáticos y aplicar migraciones
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-# Crear superusuario solo si no existe
+# Configurar el superusuario dinámicamente desde las variables de Render
 echo "from django.contrib.auth import get_user_model; \
+import os; \
 User = get_user_model(); \
-User.objects.filter(username='Alex').exists() or \
-User.objects.create_superuser('Alex', 'alexaragundi3050@gmail.com', '123456')" \
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME'); \
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD'); \
+email = 'alexaragundi3050@gmail.com'; \
+user, created = User.objects.get_or_create(username=username, defaults={'email': email}); \
+user.set_password(password); \
+user.is_superuser = True; \
+user.is_staff = True; \
+user.save(); \
+print(f'Superusuario {username} actualizado/creado con exito')" \
 | python manage.py shell
